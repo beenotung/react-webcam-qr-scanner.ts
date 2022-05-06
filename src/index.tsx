2665424dcc91b506b6799424506f87b982d3b916
Dave Lichterman <me@lavid.me>
2022-05-06 11:49:42 -0700

    v1.0.5 upgrade to React 18.x

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
+
 import './index.css'
 import App from './Demo'
 
-ReactDOM.render(
+createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
 )
